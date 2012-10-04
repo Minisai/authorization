a6557590a77db1931fe171f7f488ae17dec87584
anton.dziga <antonjyha@gmail.com>
2012-10-04 12:32:55 +0300

    Make some design changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
-  def gravatar_for(email)
-    image_tag "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?d=wavatar"
+  def gravatar_for(user)
+    image_tag "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?d=wavatar"
   end
 
 end
